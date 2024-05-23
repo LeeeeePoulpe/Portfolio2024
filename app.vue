2ecf469f68dba413f3ffc68156889aebab53dcd9
hugofollmi <hfollmi@gmail.com>
2024-05-23 19:53:32 +0200

    Try fix deploy

--- app.vue
+++ app.vue
@@ -1,37 +1,11 @@
 <template>
   <SpeedInsights/>
-  <inDevPageDesktop/>
+  <inDevPageDesktop />
 </template>
 
 <script setup>
 import {SpeedInsights} from '@vercel/speed-insights/vue';
 import inDevPageMobile from "~/views/inDevPageMobile.vue";
 import inDevPageDesktop from "~/views/inDevPageDesktop.vue";
-
-</script>
-<script>
-export default {
-  data() {
-    return {
-      windowWidth: window.innerWidth,
-    };
-  },
-
-  mounted() {
-    this.$nextTick(function () {
-      console.log('mounted');
-      window.addEventListener('resize', this.updateWindowWidth);
-    });
-  },
-
-  methods: {
-    updateWindowWidth() {
-      this.windowWidth = window.innerWidth;
-    },
-    isDesktop() {
-      return this.windowWidth > 1110;
-    },
-  },
-}
 
 </script>
