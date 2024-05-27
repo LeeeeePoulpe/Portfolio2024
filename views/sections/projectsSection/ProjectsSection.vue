af95b9d4afcb77dd48a94f36e6ad71a472276bfb
hugofollmi <hfollmi@gmail.com>
2024-05-27 22:31:40 +0200

    Add timeLine

---
+++ views/sections/projectsSection/ProjectsSection.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="min-h-screen flex flex-col justify-between projects-background p-6">
+    <div class="mb-12">
+      <div class="chat chat-start">
+        <div class="chat-bubble chat-bubble--intro">Voila la liste des projet auxquels j’ai participé ainsi que la frise chronologique des projets pour pouvoir les situer</div>
+      </div>
+      <ProjectsTimeLine/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import ProjectsTimeLine from "~/views/sections/projectsSection/ProjectsTimeLine.vue";
+
+export default {
+  name: 'ProjectsSection',
+  components: {
+    ProjectsTimeLine
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.projects-background {
+  background: linear-gradient(180deg, #AA8DC6 0%, #C9B7DA 43.5%, #F2F2F2 100%);
+}
+</style>
---
+++ views/sections/projectsSection/ProjectsTimeLine.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="flex justify-center">
+    <ul class="timeline">
+      <li>
+        <hr class="bg-primary"/>
+        <p class="timeline-start text-black">2022</p>
+        <div class="timeline-middle">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-primary"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
+        </div>
+        <div class="timeline-end timeline-box">Entrée à l’IUT</div>
+        <hr class="bg-primary"/>
+      </li>
+      <li>
+        <hr class="bg-primary"/>
+        <div class="timeline-start timeline-box">IA de tri</div>
+        <div class="timeline-middle">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-primary"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
+        </div>
+        <p class="timeline-end text-black">2022</p>
+        <hr class="bg-primary"/>
+      </li>
+      <li>
+        <hr class="bg-primary"/>
+        <p class="timeline-start text-black">2022</p>
+        <div class="timeline-middle">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-primary"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
+        </div>
+        <div class="timeline-end timeline-box">Création BD</div>
+        <hr class="bg-primary"/>
+      </li>
+      <li>
+        <hr class="bg-primary"/>
+        <div class="timeline-start timeline-box">Site CGI</div>
+        <div class="timeline-middle">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-primary"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
+        </div>
+        <p class="timeline-end text-black">2022</p>
+        <hr class="bg-primary"/>
+      </li>
+      <li>
+        <hr class="bg-primary"/>
+        <p class="timeline-start text-black">2023</p>
+        <div class="timeline-middle">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-primary"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
+        </div>
+        <div class="timeline-end timeline-box">GameJam</div>
+        <hr class="bg-primary"/>
+      </li>
+      <li>
+        <hr class="bg-primary"/>
+        <div class="timeline-start timeline-box">Sciconf</div>
+        <div class="timeline-middle">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-primary"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
+        </div>
+        <p class="timeline-end text-black">2023</p>
+        <hr class="bg-primary"/>
+      </li>
+      <li>
+        <hr class="bg-primary"/>
+        <p class="timeline-start text-black">2023</p>
+        <div class="timeline-middle">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-primary"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
+        </div>
+        <div class="timeline-end timeline-box">Arcsecond</div>
+        <hr class="bg-primary"/>
+      </li>
+      <li>
+        <hr class="bg-primary"/>
+        <div class="timeline-start timeline-box">LudoLabo</div>
+        <div class="timeline-middle">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-primary"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
+        </div>
+        <p class="timeline-end text-black">2024</p>
+        <hr class="bg-primary"/>
+      </li>
+      <li>
+        <hr class="bg-primary"/>
+        <p class="timeline-start text-black">2024</p>
+        <div class="timeline-middle">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-primary"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
+        </div>
+        <div class="timeline-end timeline-box">Carnet de stage</div>
+        <hr class="bg-primary"/>
+      </li>
+      <li>
+        <hr class="bg-primary"/>
+        <div class="timeline-start timeline-box">Stage ErgoGames</div>
+        <div class="timeline-middle">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-primary"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
+        </div>
+        <p class="timeline-end text-black">2024</p>
+        <hr class="bg-primary"/>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ProjectsTimeLine'
+}
+</script>
