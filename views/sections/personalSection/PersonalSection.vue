187f33b9e3fba92f17193a674eb9c7f376ce8908
hugofollmi <hfollmi@gmail.com>
2024-05-28 13:21:28 +0200

    Refonte personal section

--- views/sections/personalSection/PersonalSection.vue
+++ views/sections/personalSection/PersonalSection.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="min-h-screen flex flex-col justify-between items-start personal-background p-6">
+  <div class="min-h-screen flex flex-col justify-between items-start bg-slate-50 p-6">
     <div class="mb-12">
-      <div class="chat chat-start">
+      <div class="chat chat-start drop-shadow-md">
         <div class="chat-bubble chat-bubble--full">Ici vous avez mes informations personnelles</div>
       </div>
-      <PersonalCollapses/>
+      <PersonalAccordion/>
     </div>
     <div class="chat chat-start">
-      <div class="chat-bubble">
+      <div class="chat-bubble drop-shadow-md">
         Vous avez pu lire les informations me concernant. <br>
         Maintenant je vais vous présenter les différents projets que j’ai eu l’occasion de réaliser.
       </div>
@@ -18,15 +18,14 @@
 
 <script>
 import PersonalCollapses from "~/views/sections/personalSection/PersonalCollapses.vue";
+import PersonalAccordion from "~/views/sections/personalSection/PersonalAccordion.vue";
 
 export default {
   name: "PersonalSection",
-  components: {PersonalCollapses}
+  components: {PersonalAccordion, PersonalCollapses}
 }
 </script>
 
 <style lang="scss" scoped>
-.personal-background {
-  background: linear-gradient(180deg, #C5C3C3 0%, #F2F2F2 100%);
-}
+
 </style>
