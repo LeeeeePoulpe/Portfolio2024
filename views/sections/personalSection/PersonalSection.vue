5f8a39d37d139ad016170431c6ee139f92fc75ed
hugofollmi <hfollmi@gmail.com>
2024-05-28 22:36:19 +0200

    Change big screen display

--- views/sections/personalSection/PersonalSection.vue
+++ views/sections/personalSection/PersonalSection.vue
@@ -1,19 +1,20 @@
 <template>
-  <div class="min-h-screen flex flex-col justify-between items-start bg-slate-50 p-6">
-    <div class="mb-12">
-      <div class="chat chat-start drop-shadow-md">
-        <div class="chat-bubble chat-bubble--full">Ici vous avez mes informations personnelles</div>
+  <div class="min-h-screen flex justify-start xl:justify-center bg-slate-50 p-8">
+    <div class="xl:w-10/12 flex flex-col justify-between">
+      <div class="flex flex-col mb-12">
+        <div class="chat chat-start drop-shadow-md self-start">
+          <div class="chat-bubble chat-bubble--full">Ici vous avez mes informations personnelles</div>
+        </div>
+        <PersonalAccordion/>
       </div>
-      <PersonalAccordion/>
-    </div>
-    <div class="chat chat-start">
-      <div class="chat-bubble drop-shadow-md">
-        Vous avez pu lire les informations me concernant. <br>
-        Maintenant je vais vous présenter les différents projets que j’ai eu l’occasion de réaliser.
+      <div class="chat chat-start self-start justify-self-end">
+        <div class="chat-bubble drop-shadow-md">
+          Vous avez pu lire les informations me concernant. <br>
+          Maintenant je vais vous présenter les différents projets que j’ai eu l’occasion de réaliser.
+        </div>
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
@@ -22,7 +23,10 @@ import PersonalAccordion from "~/views/sections/personalSection/PersonalAccordio
 
 export default {
   name: "PersonalSection",
-  components: {PersonalAccordion, PersonalCollapses}
+  components: {
+    PersonalAccordion,
+    PersonalCollapses
+  },
 }
 </script>
 
