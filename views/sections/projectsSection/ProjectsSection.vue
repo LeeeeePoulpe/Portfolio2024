10a7ec6d45d8d0882f615675ffc62ef973076b1f
hugofollmi <hfollmi@gmail.com>
2024-05-28 21:56:46 +0200

    Add condition to show the timeline

--- views/sections/projectsSection/ProjectsSection.vue
+++ views/sections/projectsSection/ProjectsSection.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="min-h-screen flex flex-col justify-between projects-background p-6">
+  <div class="min-h-screen flex flex-col justify-between bg-slate-100 p-6">
     <div class="mb-12">
       <div class="chat chat-start">
-        <div class="chat-bubble chat-bubble--intro">Voila la liste des projet auxquels j’ai participé ainsi que la frise chronologique des projets pour pouvoir les situer</div>
+        <div class="chat-bubble chat-bubble--intro drop-shadow-md">Voila la liste des projet auxquels j’ai participé ainsi que la frise chronologique des projets pour pouvoir les situer</div>
       </div>
-      <ProjectsTimeLine/>
+      <ProjectsTimeLine v-if="isDesktop"/>
     </div>
   </div>
 </template>
@@ -17,8 +17,36 @@ export default {
   name: 'ProjectsSection',
   components: {
     ProjectsTimeLine
+  },
 
-  }
+  data() {
+    return {
+      windowWidth: typeof window !== 'undefined' ? window.innerWidth : 0,
+    };
+  },
+
+  mounted() {
+    this.$nextTick(function () {
+      window.addEventListener('resize', this.updateWindowWidth);
+    });
+  },
+
+  computed: {
+    isDesktop() {
+      return this.windowWidth > 1250;
+    },
+  },
+
+  methods: {
+    updateWindowWidth() {
+      if (typeof window !== 'undefined') {
+        this.windowWidth = window.innerWidth;
+      }
+    },
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.updateWindowWidth);
+  },
 }
 </script>
 
