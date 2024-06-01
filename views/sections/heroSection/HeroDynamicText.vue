fc76de8cee4aee9c0e4320acd3d7110997038119
hugofollmi <hfollmi@gmail.com>
2024-06-01 15:03:19 +0200

    Change option to composition

--- views/sections/heroSection/HeroDynamicText.vue
+++ views/sections/heroSection/HeroDynamicText.vue
@@ -1,24 +1,22 @@
 <template>
-  <div id="type" ref="homeDynamicText" class="md:text-6xl text-4xl hero-dynamic-text"></div>
+  <div id="type" class="md:text-6xl text-4xl hero-dynamic-text"></div>
 </template>
-<script>
+
+<script setup>
 import Typewriter from 'typewriter-effect/dist/core';
 
-export default {
-  name: 'HeroDynamicText',
 
-  mounted() {
-    const target = this.$refs.homeDynamicText;
+onMounted(() => {
+  const target = document.getElementById('type');
+  const typewriter = new Typewriter(target, {
+    loop: true,
+    autoStart: true,
+    delay: 75,
+    strings: ['Bonjour', 'Hello', 'Bonsoir', 'Salut', 'Good evening', 'Good morning', 'Good afternoon', 'Konnichiwa', 'Namaste', 'Guten Tag', 'Olá', 'Ciao-ciao', 'Hallo-hallo', 'Vanakkam', 'Yia sou', 'Kamusta'],
+    cursor: '_',
+  });
+});
 
-    const typewriter = new Typewriter(target, {
-      loop: true,
-      autoStart: true,
-      delay: 75,
-      strings: ['Bonjour', 'Hello', 'Bonsoir', 'Salut', 'Good evening', 'Good morning', 'Good afternoon', 'Konnichiwa', 'Namaste', 'Guten Tag', 'Olá', 'Ciao-ciao', 'Hallo-hallo', 'Vanakkam', 'Yia sou', 'Kamusta'],
-      cursor: '_',
-    });
-  }
-}
 </script>
 <style lang="scss" scoped>
 
