ad2a62b49ba526f6f8df6b486c2a55f9e7e25ef0
hugofollmi <hfollmi@gmail.com>
2024-06-05 18:34:40 +0200

    Add IA content

---
+++ views/sections/projectsSection/projects/IA.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="text-justify">
+    <div class="flex mb-10">
+      <div class="max-w-[700px] flex flex-col gap-4 pr-5">
+        <h1 class="font-bold text-2xl">Contexte</h1>
+        <p>
+          Ce projet est un projet de l'IUT2 de Grenoble réalisé dans le cadre de la formation BUT Informatique.
+          J'ai réalisé ce projet lors du premier semestre de la première année de la formation. Il s'agit d'un projet
+          en duo.
+        </p>
+        <p>
+          Ce projet a été réalisé lors d'une semaine spéciale bloqué pour cela. Nous avons eu une semaine pour réaliser
+          la recherche sur le Maine Learning et le développement de l'IA.
+        </p>
+        <p>
+          Cette semaine a été coupée en deux parties, la première sur les recherches d'une machine Learning, ou l'avis
+          de l'autre a été très important pour avoir une vision globale des recherches.
+          La deuxième partie était donc le développement et pour cela nous nous sommes répartis les tâches pour plus
+          d'efficacité.
+          Chaque membre du groupe avait pour responsabilité le développement d'une fonction complexe.
+        </p>
+      </div>
+      <div class="flex justify-center w-72">
+        <img src="@/assets/img/projectsSection/IA/DevIA.svg" alt="Développement d'IA" class="w-68 self-center">
+      </div>
+    </div>
+    <div class="flex flex-col gap-4 mb-10">
+      <h1 class="font-bold text-2xl">Objectifs</h1>
+      <div class="flex flex-row gap-5 items-center">
+        <img src="@/assets/img/projectsSection/IA/96.svg" alt="96%" class="w-24">
+        <p>
+          L'objectif de notre projet était de développer un programme capable de trier automatiquement des dépêches de
+          journaux en fonction du vocabulaire utilisé dans chaque article. Ce programme devait s'appuyer sur une base de
+          données d'articles déjà triés pour valider son efficacité et sa précision. En utilisant cet ensemble de
+          données préclassées, le programme devait être capable d'apprendre et d'ajuster ses critères de tri, afin
+          d'atteindre une précision de 96% dans la classification des nouveaux articles.
+          Le développement de ce programme devais être réalisé en Java.
+        </p>
+      </div>
+      <p>
+        Un autre objectif important était l'optimisation de la performance du programme. Cette optimisation visait à
+        améliorer la rapidité d'exécution et à réduire le nombre de comparaisons nécessaires pour classer chaque
+        article. Pour cela, nous devions explorer différentes techniques d'optimisation algorithmique et de gestion de
+        données, telles que l'utilisation de structures de données plus efficaces et l'application de méthodes
+        d'apprentissage automatique avancées.
+      </p>
+      <p>
+        En résumé, notre projet avait pour objectifs principaux de développer un programme de tri des dépêches de
+        journaux à haute précision et de l'optimiser pour qu'il soit rapide et efficace, répondant ainsi aux besoins
+        pratiques du journalisme et de la gestion de l'information.
+      </p>
+    </div>
+    <div class="flex flex-col gap-4 mb-10">
+      <h1 class="font-bold text-2xl">Résultats</h1>
+      <p>
+        Notre projet avait pour objectif le développement d'un programme performant capable de trier des centaines
+        d'articles
+        de journaux en environ 1 seconde, avec une précision de 96%. Nous avons réussi à atteindre cet objectif en
+        créant un programme efficace et rapide, capable de traiter un grand volume d'articles avec une haute précision.
+      </p>
+      <p>
+        En plus de cela, nous avons produit la documentation requise en anglais et présenté oralement les résultats de
+        notre travail. Ce projet m'a particulièrement intéressé car il m'a permis de découvrir le machine learning et le
+        tri de texte. Je suis convaincu que nous avons bien réussi, en grande partie grâce à une répartition efficace
+        des tâches et à une communication fluide au sein de notre duo.
+      </p>
+    </div>
+    <div class="divider m-0 divider-neutral"></div>
+    <div>
+      <h1 class="font-bold text-2xl">Conclusion</h1>
+      <div class="flex flex-row items-center gap-5">
+        <img src="@/assets/img/projectsSection/ErgoGames/CheckList.svg" alt="" class="w-24 h-24">
+        <div>
+          <p class="mb-3">
+            L'objectif de cette semaine était de nous familiariser avec le travail en duo, notre premier projet de
+            l'année. Bien que j'étais déjà habitué au travail en équipe, ce projet m'a permis de m'améliorer davantage.
+
+            Nous avons aussi appris à utiliser le machine learning, ce qui était très intéressant, et j'ai pu appliquer
+            les méthodes de travail vues en cours.
+          </p>
+          <p>
+            Cette semaine a été enrichissante et nous a permis d'acquérir de nouvelles compétences pratiques.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
