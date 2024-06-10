ae23618e2025518bddb0ae73657eebcbd4b3774c
hugofollmi <hfollmi@gmail.com>
2024-06-10 08:56:23 +0200

    Remove useless code

--- app.vue
+++ app.vue
@@ -8,22 +8,4 @@
 import {SpeedInsights} from '@vercel/speed-insights/vue';
 import HomePage from './views/HomePage.vue';
 import InProgressPage from "~/views/inDevPage/InProgressPage.vue";
-
-const windowWidth = ref(typeof window !== 'undefined' ? window.innerWidth : 0);
-
-const updateWindowWidth = () => {
-  if (typeof window !== 'undefined') {
-    windowWidth.value = window.innerWidth;
-  }
-};
-
-onMounted(() => {
-  window.addEventListener('resize', updateWindowWidth);
-});
-
-onBeforeUnmount(() => {
-  window.removeEventListener('resize', updateWindowWidth);
-});
-
-const isDesktop = computed(() => windowWidth.value > 800);
 </script>
