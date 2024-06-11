7cd7ef2b1a46bfc86889cda5452544147d81fce7
hugofollmi <hfollmi@gmail.com>
2024-06-11 20:42:20 +0200

    Fix projects

--- views/sections/projectsSection/projects/GameJam.vue
+++ views/sections/projectsSection/projects/GameJam.vue
@@ -7,19 +7,19 @@
           La
           <a href="https://fr.wikipedia.org/wiki/Game_jam" target="_blank"
              class="link link-info no-underline transition-colors"><b>GameJam</b></a>
-          est un évènement national durant lequel les étudiants Doivent réaliser un jeu vidéo avec des
+          est un évènement national durant lequel les étudiants doivent réaliser un jeu vidéo avec des
           contraintes en une semaine. Ce projet a été réalisé dans le cadre de la formation BUT Informatique à l'IUT2
           de Grenoble lors du deuxième semestre. Nous étions une équipe de quatre étudiants.
         </p>
         <p>
-          Nous devions réaliser un jeu vidéo en une semaine sur le thème "Le merveilleux monde de l'IUT2".
-          Nous avions le choix du style de jeu. Nous avons donc décidé de réaliser un jeu dans le style de Pokémon, se
-          passant au premier étage de l'IUT où chaque salle représente une salle de bosse. Ce jeu, nous l'avons appelé
+          Nous devions réaliser un jeu vidéo sur le thème "Le merveilleux monde de l'IUT2".
+          Nous avions le choix du style de jeu : nous avons donc décidé de réaliser un jeu dans le style de Pokémon, se
+          passant au premier étage de l'IUT où chaque salle représente une salle de boss. Ce jeu, nous l'avons appelé
           <a href="https://github.com/sadspear/gamejam2023" target="_blank"
              class="link link-info no-underline transition-colors"><b>Time striker</b></a>.
         </p>
         <p>
-          Les contraintes imposés lors de cette GameJam était de développer en
+          Les contraintes imposées lors de cette GameJam était de développer en
           <a href="https://developer.mozilla.org/fr/docs/Glossary/Python" target="_blank"
              class="link link-info no-underline transition-colors"><b>Python</b></a> avec la bibliothèque
           <a href="https://www.pygame.org/" target="_blank" class="link link-info no-underline transition-colors"><b>PyGame</b></a>.
@@ -35,21 +35,21 @@
     <div class="flex flex-col gap-4 mb-10">
       <h1 class="font-bold text-2xl">Déroulé</h1>
       <p>
-        Notre objectif était de réaliser un jeu vidéo en une semaine sur le thème "Le merveilleux monde de l'IUT2".
-        Le plus gros défi était de réaliser un jeu vidéo en une semaine. Il y avait tout à faire, de la réflexion à la
-        conception à la réalisation. Nous devions également travailler en équipe pour réaliser ce projet.
+        Tout était à faire, de la réflexion à la conception à la réalisation. Nous devions également travailler
+        en équipe pour réaliser ce projet.
       </p>
       <p>
-        Objectif de la GameJam était aussi de nous faire apprendre le python au sein de l'IT, car c'est une matière
-        qui n'est pas dans enseigner par défaut.
-        Travailler quatre jours consécutifs sur une technologie. Nous connaissions pas été un réel défi, mais aussi
-        une expérience très très enrichissante.
+        Objectif de la GameJam était aussi de nous faire apprendre le python au sein de l'IUT, car c'est une matière
+        qui n'est pas enseigné dans le curcus.
+        Travailler quatre jours consécutifs sur une nouvelle technologie que nous ne connaissions pas était un réel défi,
+        mais aussi une expérience enrichissante.
       </p>
       <p>
-        Le développement du jeu se réalisent en python, mais pour ce qui est de l'affichage, nous devions utiliser
-        des tuiles.
-        Personnellement, je me suis occupé au début de réaliser l'intégralité du niveau avec l'étage de l'IUT et
-        toutes les salles de bosse.
+        Le développement du jeu à été réalisé en
+        <a href="https://developer.mozilla.org/fr/docs/Glossary/Python" target="_blank" class="link link-info no-underline transition-colors"><b>Python</b></a>,
+        mais pour ce qui est de l'affichage, nous devions utiliser
+        des tuiles. Personnellement, je me suis occupé au début de réaliser l'intégralité du niveau avec l'étage de l'IUT et
+        toutes les salles de boss.
         Après cela, j'ai eu l'occasion de pouvoir pouvoir m'occuper des fonctions d'animation et de déplacement du
         joueur.
       </p>
@@ -58,7 +58,7 @@
       <h1 class="font-bold text-2xl">Résultats</h1>
       <p>
         Notre jeu reprend donc le principe de Pokémon, où le joueur doit affronter des ennemis dans des combats à l'aide
-        d'une barre de rythme pour attaquer. Le joueur peut également se soigner en allant au toilette. Pour sortir de
+        d'une barre de rythme pour attaquer. Le joueur peut également se soigner en allant aux toilettes. Pour sortir de
         l'IUT, le joueur doit récupérer les clés de l'IUT et vaincre le boss final : son diplôme. Les clés sont cachées
         dans les salles de cours qui chacune représente un partiel.
       </p>
--- views/sections/projectsSection/projects/Sciconf.vue
+++ views/sections/projectsSection/projects/Sciconf.vue
@@ -6,11 +6,8 @@
       <div class="max-w-[700px] flex flex-col gap-4">
         <h1 class="font-bold text-2xl">Contexte</h1>
         <p>
-          Ce projet, réalisé dans le cadre de la formation BUT Informatique à l'IUT2 de Grenoble
-          lors du deuxième semestre, s'est déroulé à cinq.
-        </p>
-        <p>
-          Nous devions créer une application de gestion d'évènements. Nous avions le choix sur le type
+          Lors du second semestre du BUT informatique à l'IUT2 de Grenoble, nous avons réalisé par groupe de cinq
+          un projet visant à développer une application de gestion d'évènements. Nous avions le choix sur le type
           d'évènement. Nous avons donc choisi pour notre groupe les conférences scientifiques. L'application devait
           permettre de gérer les conférences, les intervenants, les participants et les salles.
         </p>
@@ -32,20 +29,14 @@
       <h1 class="font-bold text-2xl">Objectifs</h1>
       <div class="flex flex-col gap-5 items-center">
         <p>
-          Notre objectif était de développer une application de gestion d'évènements dans les contraintes avec
-          <a href="https://developer.mozilla.org/fr/docs/Glossary/Java" target="_blank" class="link link-info no-underline transition-colors"><b>Java</b></a> et
-          <a href="https://openjfx.io/" target="_blank" class="link link-info no-underline transition-colors"><b>JavaFX</b></a>.
-          De plus, nous avions le choix du style d'évènement. Nous avons donc choisi les conférences
-          scientifiques. L'objectif du projet est de nous faire comprendre toutes les étapes d'un projet de la
-          réflexion à la conception à la réalisation. Nous devions également travailler en équipe pour réaliser ce
-          projet.
+          L'objectif du projet était de nous faire comprendre toutes les étapes du déroulé d'un projet : de la
+          réflexion à la réalisation en passant par la conception.
         </p>
         <img src="@/assets/img/projectsSection/Sciconf/Maquette.svg" alt="96%"
              class="w-[600px] rounded-xl drop-shadow-sm">
         <p>
           Nous avons donc commencé par réfléchir à l'application que nous voulions réaliser. Nous avons ensuite
-          réalisé une maquette de l'application pour nous aider à la réaliser. Le développement a été l'étape suivante
-          et a été principalement guidé par les maquettes que nous avions réalisé au préalable. Enfin, nous avons
+          réalisé une maquette pour nous aider. Le développement a été l'étape suivante. Enfin, nous avons
           réalisé des tests pour vérifier le bon fonctionnement de l'application.
         </p>
       </div>
@@ -53,12 +44,11 @@
     <div class="flex flex-col gap-4 mb-10">
       <h1 class="font-bold text-2xl">Résultats</h1>
       <p>
-        L'application finale est conforme aux maquettes et aux contraintes demandées par l'IUT. Nous avons réussi à
-        réaliser une application de gestion de conférences scientifiques. L'application permet de gérer les conférences,
-        les intervenants, les participants et les salles. Toutes ces choses, nous l'avions annoncé dès le début, nous
-        avons réussi à les développer. Nous avons réussi à développer cette application grâce à notre travail d'équipe
-        et à notre répartition équitable des tâches en fonction des compétences de chacun. Nous avons également réussi
-        à respecter les délais imposés par l'IUT.
+        L'application finale est conforme aux maquettes et aux contraintes demandées par l'IUT.
+        L'application permet de gérer les conférences,
+        les intervenants, les participants et les salles. Nous avons réussi à développer cette application grâce à notre
+        travail d'équipe et à notre répartition équitable des tâches en fonction des compétences de chacun. Nous avons
+        également réussi à respecter les délais imposés par l'IUT.
       </p>
     </div>
     <div class="divider m-0 divider-neutral"></div>
@@ -67,10 +57,9 @@
       <div class="flex flex-row items-center gap-5">
         <img src="@/assets/img/projectsSection/Sciconf/Conclu.svg" alt="" class="w-24 drop-shadow-sm">
         <p>
-          En conclusion, ce projet nous a permis de comprendre toutes les étapes d'un projet de la réflexion à la
-          conception à la réalisation. Nous avons également appris à travailler en équipe pour réaliser un projet. Nous
-          avons également appris à respecter les délais imposés par l'IUT. Ce projet nous a permis de mettre en pratique
-          les connaissances acquises lors de notre formation. Nous avons également appris à travailler avec
+          En conclusion, ce projet nous a permis de comprendre toutes les étapes d'un projet. Nous avons également
+          appris à travailler en équipe pour réaliser un projet. Ce projet nous a permis de mettre en pratique
+          les connaissances acquises lors de notre formation tel que :
           <a href="https://developer.mozilla.org/fr/docs/Glossary/Java" target="_blank" class="link link-info no-underline transition-colors"><b>Java</b></a> et
           <a href="https://openjfx.io/" target="_blank" class="link link-info no-underline transition-colors"><b>JavaFX</b></a>.
         </p>
